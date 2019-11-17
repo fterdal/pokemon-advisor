8e22039139c416c9a5715d8898395499279dfdef
Finn <finnterdal@gmail.com>
2019-11-17 15:51:22 -0600

    inertial kermit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const Main = () => {
+  return <h1>Pokémon Type Advisor</h1>
+}
+
+ReactDOM.render(<Main />, document.getElementById("main"))
