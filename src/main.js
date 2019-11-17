bfdaf2f646b8bc86a6d6652476f20245c93b4982
Finn <finnterdal@gmail.com>
2019-11-17 16:47:40 -0600

    gotta list em all

--- src/main.js
+++ src/main.js
@@ -1,8 +1,38 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+const types = [
+  "bug",
+  "dark",
+  "dragon",
+  "electric",
+  "fairy",
+  "fighting",
+  "fire",
+  "flying",
+  "ghost",
+  "grass",
+  "ground",
+  "ice",
+  "normal",
+  "poison",
+  "psychic",
+  "rock",
+  "steel",
+  "water"
+]
+
 const Main = () => {
-  return <h1>Pokémon Type Advisor</h1>
+  return (
+    <>
+      <h1>Pokémon Type Advisor</h1>
+      <ul className="types-list">
+        {types.map(type => (
+          <li key={type}>{type}</li>
+        ))}
+      </ul>
+    </>
+  )
 }
 
 ReactDOM.render(<Main />, document.getElementById("main"))
