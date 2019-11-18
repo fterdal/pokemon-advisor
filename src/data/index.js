13b722bd21330e52a124c3343f75664eede0f9b4
Finn <finnterdal@gmail.com>
2019-11-18 10:25:39 -0600

    simplify data import

---
+++ src/data/index.js
@@ -0,0 +1,39 @@
+import bug from "./type-bug.json"
+import dark from "./type-dark.json"
+import dragon from "./type-dragon.json"
+import electric from "./type-electric.json"
+import fairy from "./type-fairy.json"
+import fighting from "./type-fighting.json"
+import fire from "./type-fire.json"
+import flying from "./type-flying.json"
+import ghost from "./type-ghost.json"
+import grass from "./type-grass.json"
+import ground from "./type-ground.json"
+import ice from "./type-ice.json"
+import normal from "./type-normal.json"
+import poison from "./type-poison.json"
+import psychic from "./type-psychic.json"
+import rock from "./type-rock.json"
+import steel from "./type-steel.json"
+import water from "./type-water.json"
+
+export default {
+  bug,
+  dark,
+  dragon,
+  electric,
+  fairy,
+  fighting,
+  fire,
+  flying,
+  ghost,
+  grass,
+  ground,
+  ice,
+  normal,
+  poison,
+  psychic,
+  rock,
+  steel,
+  water
+}
